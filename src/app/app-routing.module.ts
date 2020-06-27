8280bca0128df3bc27778544faaa905a28ca2547
Ashwin <ashwin.s095@gmail.com>
2020-06-28 03:30:25 +0530

    latest

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NewsFeedComponent } from './news-feed/news-feed.component';
+import { BookMarkComponent } from './book-mark/book-mark.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: "bookmarks",
+    component: BookMarkComponent
+  },
+  {
+    path: "home",
+    component: NewsFeedComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
